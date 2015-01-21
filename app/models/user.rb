6a10420910d1155d9f29678b07a840cacd28c3fc
Kit <dev_kit@icloud.com>
2015-01-21 10:01:57 -0700

    The Hound has bitten

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 class User < ActiveRecord::Base
   validates :first_name, :last_name, presence: true
-  validates :email, format: { 
-    with: /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/ 
-    }, uniqueness: true
-  validates :user_name, length: { 
-    in: 2..23, 
-    too_long: "%{count} is the alowed number of characters" },
-    allow_blank: true
+  validates :email, 
+            format: {
+              with: /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/
+            },
+            uniqueness: true
+  validates :user_name, 
+            length: { in: 2..23,
+              too_long: "%{count} is the alowed number of characters"
+            },
+            allow_blank: true
 end
