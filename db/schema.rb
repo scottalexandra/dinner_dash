04cc71a0fdd31d61469ca0e76ccbee74d90335da
Alex Robinson <alexandra.scott335@gmail.com>
2015-01-21 15:38:00 -0700

    created order relationships to items, user, and added status

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150121181740) do
+ActiveRecord::Schema.define(version: 20150121222413) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,8 +48,10 @@ ActiveRecord::Schema.define(version: 20150121181740) do
   add_index "order_items", ["order_id"], name: "index_order_items_on_order_id", using: :btree
 
   create_table "orders", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
+    t.integer  "user_id"
+    t.string   "status",     default: "ordered"
   end
 
   create_table "users", force: :cascade do |t|
