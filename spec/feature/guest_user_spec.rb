78127ee05636109fc18be267b7176016b66285d2
Bryce Holcomb <bryce_holcomb@outlook.com>
2015-01-21 22:02:21 -0700

    finish guest can browse categories and items tests and implementation

--- spec/feature/guest_user_spec.rb
+++ spec/feature/guest_user_spec.rb
@@ -10,19 +10,38 @@ describe "An unauthenticated user" do
                            description: "The classic breakfast dish",
                            price: 1000)
     category2.items.create(title: "BLT",
-                           description: "The classic lunch disk",
+                           description: "The classic lunch dish",
                            price: 1000)
+    visit root_path
   end
 
   it "can browse all items" do
-    visit root_path
-    click_link_or_button "Menu"
-    expect(current_path).to eq(categories_path)
-    within("#menu") do
-      expect(page).to have_content category1.name
+    click_link_or_button "Browse all Items"
+    expect(current_path).to eq(items_path)
+    within("div.items") do
+      within("div#item-1") do
+        expect(page).to have_content "Bacon and Eggs"
+        expect(page).to have_content "The classic breakfast dish"
+      end
+      within("div#item-2") do
+        expect(page).to have_content "BLT"
+        expect(page).to have_content "The classic lunch dish"
+      end
     end
-    within("ul#menu-categories") do
-      expect(page).to have_content "Bacon and Eggs"
+  end
+
+  it "can browse by categories" do
+    click_link_or_button "Browse by Categories"
+    expect(current_path).to eq(categories_path)
+    within("div.categories") do
+      within("div#category-1") do
+        expect(page).to have_content category1.name
+        expect(page).to have_content "Bacon and Eggs"
+      end
+      within("div#category-2") do
+        expect(page).to have_content category2.name
+        expect(page).to have_content "BLT"
+      end
     end
   end
 end
