c3488b9f307b62500754234f7378c3d086831fe2
Bryce Holcomb <bryce_holcomb@outlook.com>
2015-01-22 17:58:56 -0700

    fix hash structure in admin test

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -3,6 +3,6 @@ class Admin < ActiveRecord::Base
 
   validates :first_name, :last_name, :email, presence: true
   validates :email, format: {
-              with: /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/
+    with: /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/
   }, uniqueness: true
 end
--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -11,42 +11,42 @@ RSpec.describe Admin, :type => :model do
 
   it "is not valid without a first and last name" do
     invalid_admin1 = Admin.new(first_name: "",
-                      last_name: "Last",
-                      email: "admin@gmail.com",
-                      password: "password")
+                               last_name: "Last",
+                               email: "admin@gmail.com",
+                               password: "password")
     invalid_admin2 = Admin.new(first_name: "First",
-                      last_name: "",
-                      email: "admin@gmail.com",
-                      password: "password")
+                               last_name: "",
+                               email: "admin@gmail.com",
+                               password: "password")
     expect(invalid_admin1).to_not be_valid
     expect(invalid_admin2).to_not be_valid
   end
 
   it "is not valid without an email" do
     invalid_admin1 = Admin.new(first_name: "First",
-                      last_name: "Last",
-                      email: "",
-                      password: "password")
+                               last_name: "Last",
+                               email: "",
+                               password: "password")
     expect(invalid_admin1).to_not be_valid
   end
 
   it "is not valid without a properly formatted email" do
     invalid_admin1 = Admin.new(first_name: "First",
-                      last_name: "Last",
-                      email: "kitATkit.com",
-                      password: "password")
+                               last_name: "Last",
+                               email: "kitATkit.com",
+                               password: "password")
     expect(invalid_admin1).to_not be_valid
   end
 
   it "is not valid with a duplicate email" do
-    invalid_admin1 = Admin.create(first_name: "First",
-                      last_name: "Last",
-                      email: "kit@kit.com",
-                      password: "password")
+    Admin.create(first_name: "First",
+                 last_name: "Last",
+                 email: "kit@kit.com",
+                 password: "password")
     invalid_admin2 = Admin.new(first_name: "First",
-                      last_name: "Last",
-                      email: "kit@kit.com",
-                      password: "password")
+                               last_name: "Last",
+                               email: "kit@kit.com",
+                               password: "password")
     expect(invalid_admin2).to_not be_valid
   end
 end
