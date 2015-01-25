ff9a28c4a0e9afddee6b0799516a3133e8edda9c
Kit <dev_kit@icloud.com>
2015-01-25 14:31:34 -0700

    add admin create and new page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+# before_action :current_user?, only: [:show]
+
   def show
     @user = User.find(params[:id])
     authorize! :read, @user
@@ -11,15 +13,23 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to user_path(@user.id), alert: "User created successfully."
+      session[:user_id] = @user.id
+      redirect_to user_path(@user.id), notice: "User created successfully."
     else
-      redirect_to new_user_path, alert: "Error creating user."
+      redirect_to new_user_path, error: "Invalid Login Credentials"
     end
   end
 
   private
 
   def user_params
-    params.require(:user).permit!
+    params.require(:user).permit(:first_name, :last_name, :email,
+                                 :display_name, :password)
   end
+
+  # def current_user?
+  #   unless current_user[:id] == params[:id].to_i
+  #     redirect_to root_path
+  #   end
+  # end
 end
